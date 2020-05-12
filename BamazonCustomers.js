83f8eddb9c45e8cfb20e8615737e4e10222ee191
MXN <engineerandres@gmail.com>
2020-05-11 20:30:04 -0700

    i was able to display the items from the database onto the command line

--- BamazonCustomers.js
+++ BamazonCustomers.js
@@ -8,29 +8,29 @@ var connection = mysql.createConnection({
   password: "234165",
   database: "bamazon",
 });
+
 console.log("here is what is available for sale: Buy something and get out!"); // bamazon is not as friendly as amazon.
+function showEverthing() {
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log(
+        "ID: " +
+          res[i].ITEM_ID +
+          " Product: " +
+          res[i].Product_Name +
+          "            Department Name: " +
+          res[i].Department_Name +
+          "              Price " +
+          "$" +
+          res[i].Price +
+          "            Stock Quantity " +
+          res[i].Stock_Quantity
+      );
+    }
+
+    connection.end();
+  });
+}
 
 showEverthing();
-
-function showEverthing() {
-  connection.query("Select * FROM bamazon.products"),
-    function (err, res) {
-      if (err) throw err;
-
-      for (var i = 0; i < res.length; i++) {
-        console.log(
-          "ID: " +
-            res[i].ITEM_ID +
-            " Product: " +
-            res[i].Product_Name +
-            " Department Name: " +
-            res[i].Department_Name +
-            " Price " +
-            "$" +
-            res[i].Price +
-            " Stock Quantity " +
-            res[i].Stock_Quantity
-        );
-      }
-    };
-}
